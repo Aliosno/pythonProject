69193e2e592d093d44da9d3711b611a04900aef4
KOLOBOK5544 <alisasosno@gmail.com>
2022-04-15 13:45:30 +0300

    Add file

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,10 +1,10 @@
 n_tiket=abs(int(input('Введите количество посетителей = ')))
 
-List_age = list(map(int, input(f'введите возраст посетителей через запятую, всего {n_tiket} билетов = ').split(',')))
-print(List_age)
+list_age = list(map(int, input(f'введите возраст посетителей через запятую, всего {n_tiket} билетов = ').split(',')))
+print(list_age)
 
 summ = 0
-for i in List_age :
+for i in list_age :
 
  if i < 18:
     summ += 0
@@ -15,7 +15,7 @@ for i in List_age :
  elif 25<i :
      summ += 1390
 
- if n_tiket > 3 :
+if n_tiket > 3 :
      summ *= 0.9
 
 print(summ)
